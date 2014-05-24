c606ea0e039d03c5a833d0637c9354195cb9ab43
Fernando-Oliva <fernando.oliva.hueto@gmail.com>
2014-05-24 12:26:32 +0200

    Control green color and red color

--- Amnesia/MainWindow.xaml.cs
+++ Amnesia/MainWindow.xaml.cs
@@ -18,26 +18,6 @@ using System.Globalization;
 namespace Amnesia
 {
 
-    public class ValueColorConverter : IValueConverter
-    {
-        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
-        {
-            var str = value as string;
-            if (str == null) return null;
-
-            int intValue;
-            if (!int.TryParse(str, out intValue)) return null;
-
-            if (intValue <= 1) return Brushes.Red;
-            else if (intValue <= 2) return Brushes.Yellow;
-            else return Brushes.Green;
-        }
-
-        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
-        {
-            throw new NotImplementedException();
-        }
-    }
     /// <summary>
     /// Lógica de interacción para MainWindow.xaml
     /// </summary>
@@ -63,13 +43,36 @@ namespace Amnesia
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
-            DataGridRow row = dgTask.ItemContainerGenerator.ContainerFromItem(dgTask.Items[0]) as DataGridRow;
-            DataGridCell cell = dgTask.Columns[2].GetCellContent(row).Parent as DataGridCell;
+            DataGridRow row;
+            int completedColumnIndex = 2;
+            int testColumnIndex = 3;
+            int aprovedColumnIndex = 4;
 
+            for (int i = 0; i < dgTask.Items.Count; i++)
+            {
+                row  = dgTask.ItemContainerGenerator.ContainerFromItem(dgTask.Items[i]) as DataGridRow;
+
+                TaskColumnColorSet(row, completedColumnIndex);
+                TaskColumnColorSet(row, testColumnIndex);
+                TaskColumnColorSet(row, aprovedColumnIndex);
+            }
+        }
+
+        private void TaskColumnColorSet(DataGridRow row, int columnIndex)
+        {
+            DataGridCell cell = dgTask.Columns[columnIndex].GetCellContent(row).Parent as DataGridCell;
             TextBlock content = cell.Content as TextBlock;
 
             if (content.Text.Equals("verde"))
+            {
+                content.Text = string.Empty;
                 cell.Background = Brushes.LightGreen;
+            }
+            else if (content.Text.Equals("rojo"))
+            {
+                content.Text = string.Empty;
+                cell.Background = Brushes.Red;
+            }
         }
     }
 }
